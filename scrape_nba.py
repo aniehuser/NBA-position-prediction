e13888d5c5c85db7379e9895f551a13c1e770183
Carl Lundin <carllundin55@gmail.com>
2018-04-12 10:59:28 -0700

    made some minor changes

--- scrape_nba.py
+++ scrape_nba.py
@@ -7,11 +7,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-import re
 import pandas as pd
-import requests
 
-directory = r"/Users/anthonyniehuser/Desktop/temp-cpsc410/NBA-position-prediction/chromedriver"
 url = "http://stats.nba.com/leaders/"
 
 def main():
